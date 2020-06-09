e6f8f3d773031da7358af295230879b3a2707f63
及川裕矢 <oikawa@oikawayuuyanoMacBook-Pro.local>
2020-06-09 23:07:38 +0900

    作品の概要説明としてREADMEを記載した

--- src/js/Enemy.js
+++ src/js/Enemy.js
@@ -6,7 +6,7 @@ function setEnemy(){
       addMessage(enemy.name + " が現れた");
       break;
     case 1:
-      enemy = new Enemy("親玉", 120, 5,4);
+      enemy = new Enemy("親玉", 120, 2,3);
       enemyDeckCard=["type1","type1","type1","type1","type1","type1","type2","type2","type2","type3","type0","type5"];
       addMessage(enemy.name + " が仇討ちに現れた！");
       break;
